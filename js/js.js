12d84e81fe4e1a1f0a74619416408765901d0188
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2022-07-03 13:27:52 +0300

    Basic JS les7 complete

--- js/js.js
+++ js/js.js
@@ -21,3 +21,14 @@ var myFirstName = "Vladimir",
     myLastName = "Zvyagin";
 
 // Understanding Uninitialized Variables
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// Understanding Case Sensitivity in Variables
