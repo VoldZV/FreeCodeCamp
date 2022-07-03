71a80917f25cce2502e67d9dbdc168f11104bc87
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2022-07-03 13:12:45 +0300

    Basic JS les4 complete

--- js/js.js
+++ js/js.js
@@ -6,3 +6,11 @@ a = 7;
 // Only change code below this line
 
 // Assigning the Value of One Variable to Another
+// Setup
+var a;
+a = 7;
+var b;
+b = a;
+// Only change code below this line
+
+// Initializing Variables with the Assignment Operator
