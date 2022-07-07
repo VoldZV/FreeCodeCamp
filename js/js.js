79ee43f5aa2b766f16d3ed5c7ca562d4051dee5b
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2022-07-07 23:03:21 +0300

    JS les60 complete

--- js/js.js
+++ js/js.js
@@ -100,3 +100,68 @@ a += 12;
 b += 9;
 c += 7;
 
+//Compound Assignment With Augmented Subtraction
+
+//Escaping Literal Quotes in Strings
+const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
+//Quoting Strings with Single Quotes
+
+// Escape Sequences in Strings
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"
+
+// Concatenating Strings with Plus Operator
+const myStr = "This is the start." +" " + "This is the end.";
+let myStr = "This is the first sentence.";
+myStr += " " + "This is the second sentence."
+
+// Constructing Strings with Variables
+const myName = "Vladimir";
+const myStr = "My name is " + myName +" and I am well!";
+
+// Appending Variables to Strings
+const someAdjective = "123";
+let myStr = "Learning to code is ";
+myStr += someAdjective
+
+// Find the Length of a String
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+// Only change code below this line
+lastNameLength = lastName.length;
+
+// Use Bracket Notation to Find the First Character in a String
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+// Access Array Data with Indexes
+const myArray = [50, 60, 70];
+const myData = myArray[0]
+
+// Return a Value from a Function with Return
+function timesFive (num) {
+    return num*5
+  }
+
+//   Assignment with a Returned Value
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+processed = processArg(7)
+
+// Comparison with the Equality Operator
+function testEqual(val) {
+    if (val == 12) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  testEqual(10);
