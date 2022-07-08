cb77ac4c724e110916b8b7eb6b62742017cb4743
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2022-07-08 21:33:27 +0300

    JS les82 complete

--- js/js.js
+++ js/js.js
@@ -165,3 +165,44 @@ function testEqual(val) {
   }
   
   testEqual(10);
+
+
+//   Counting Cards
+  let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  let bet = ' Bet';
+  let hold = ' Hold'
+  
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    count++;
+    break
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+    count--;
+    break;
+    default:
+    break;
+  }
+
+  if (count > 0) {
+    return count + bet
+  } else {
+    return count + hold
+  }
+  return "Change Me";
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+
